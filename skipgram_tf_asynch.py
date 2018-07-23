9aea9f5e9ee0ea6d9864412dcdc9261a9ca499d5
ducdauge <ep490@cam.ac.uk>
2018-07-23 12:18:59 +0100

    Update skipgram_tf_asynch.py

--- skipgram_tf_asynch.py
+++ skipgram_tf_asynch.py
@@ -1,5 +1,5 @@
 #################################################################
-# Parts of the code are sourced from the TF word2vec example:   # 
+# Parts of the code are sourced from the TF word2vec example:   #
 # https://www.tensorflow.org/tutorials/word2vec                 #
 #################################################################
 
@@ -9,16 +9,26 @@ from __future__ import print_function
 
 import random
 import numpy
-import cPickle as pkl
+import os
 
 import tensorflow as tf
 from utils import TreebankReader, cw_Tokenizer
 import logging
-
 import threading
 
 FORMAT = "%(asctime)s - %(levelname)s - %(message)s"
 logging.basicConfig(format=FORMAT, level=logging.DEBUG)
+
+flags = tf.flags
+flags.DEFINE_string ('corpus_path', 'file.conll', 'full path for conll file')
+flags.DEFINE_integer('embedding_size', 300, 'embedding size')
+flags.DEFINE_integer('min_freq', 100, 'minimun word frequency (discarded if lower)')
+flags.DEFINE_integer('negative_samples', 100, 'number of negative samples for contrastive loss')
+flags.DEFINE_integer('epochs', 10, 'number of epochs')
+flags.DEFINE_integer('batch_size', 16, 'batch size')
+flags.DEFINE_float('learning_rate', 0.2, 'learning rate')
+flags.DEFINE_boolean('labeled', False, 'concatenate dependency relations to WEs?')
+FLAGS = flags.FLAGS
 
 class CustomRunner(object):
     """
@@ -37,9 +47,6 @@ class CustomRunner(object):
                                            capacity=capacity,
                                            min_after_dequeue=batch_size)
 
-        # The symbolic operation to add data to the queue
-        # we could do some preprocessing here or do it in numpy. In this example
-        # we do the scaling in numpy
         self.enqueue_op = self.queue.enqueue_many([self.words_ph, self.contexts_ph])
 
     def get_inputs(self):
@@ -77,13 +84,13 @@ class SkipGram(object):
     embedding_size (int): the size of the neural embeddings
     labeled (boolean): whether including dependency labels in contexts
     """
-    
-    def __init__(self, corpus_path, embedding_size=300, num_words=200000,
-                 negative_samples=15, epochs=15, batch_size=16, learning_rate=0.2,
-		 labeled=True, *args, **kwargs):
+
+    def __init__(self, corpus_path, embedding_size=300, min_freq=100,
+                 negative_samples=100, epochs=10, batch_size=16, learning_rate=0.2,
+		 labeled=False, *args, **kwargs):
         self.corpus_path = corpus_path
         self.embedding_size = embedding_size
-        self.num_words = num_words
+        self.min_freq = min_freq
         self.negative_samples=negative_samples
         self.epochs=epochs
         self.batch_size = batch_size
@@ -92,61 +99,59 @@ class SkipGram(object):
         self.corpus = None
         self.model = None
         self.labeled = labeled
-        self._words = 0
-        super(SkipGram, self).__init__(*args, **kwargs)   
+        super(SkipGram, self).__init__(*args, **kwargs)
 
     def tokenize_corpus(self):
         """ Tokenize the corpus using Keras helper classes"""
-        self.corpus = TreebankReader(self.corpus_path, labeled = self.labeled)
+        self.corpus = TreebankReader(self.corpus_path, labeled=self.labeled)
         logging.info('Tokenizing the corpus')
-        self.tokenizer = cw_Tokenizer(num_words=self.num_words)
-        for sentence in self.corpus.generate(single=True):        
-            self.tokenizer.fit_on_texts(sentence)
-        self.tokenizer.finalize_dict()
-        logging.info('Vocabulary length is {} for words and {} for contexts'.format(
-                len(self.tokenizer.word_index), len(self.tokenizer.context_index)))
+        self.tokenizer = cw_Tokenizer(min_freq=self.min_freq)
+        for words in self.corpus.generate(single=True):
+            self.tokenizer.fit_on_texts(words)
+        self.tokenizer.finalize_dict(self.corpus._wordcounts)
 
     def skipgrams_generator(self, sampling_table):
         words = []
         contexts = []
         for s in self.corpus.generate():
-            for w, c in s:                
+            for w, c in s:
                 wi = self.tokenizer.word_index.get(w)
                 if not wi:
                     continue
+                ci = self.tokenizer.context_index.get(c, 0)
                 if sampling_table is not None:
-                    if sampling_table[wi] < random.random():
+                    if sampling_table[ci] < random.random():
                         continue
-                di = self.tokenizer.context_index.get(c, 0)
                 words.append(wi)
-                contexts.append(di)
-                if len(words) == self.batch_size:             
-                    yield (numpy.array(words, dtype=numpy.int32), 
+                contexts.append(ci)
+                if len(words) == self.batch_size:
+                    yield (numpy.array(words, dtype=numpy.int32),
                            numpy.array(contexts, dtype=numpy.int32))
                     words = []
                     contexts = []
-                    
+
     @staticmethod
-    def _make_sampling_table(size, sampling_factor=1e-4):
+    def _make_sampling_table(size, sampling_factor=1e-5):
         gamma = 0.577
         rank = numpy.arange(size)
         rank[0] = 1
         inv_fq = rank * (numpy.log(rank) + gamma) + 0.5 - 1. / (12. * rank)
         f = sampling_factor * inv_fq
+
         return numpy.minimum(1., f / numpy.sqrt(f))
 
     def prepare_model(self):
         vocab_length = len(self.tokenizer.word_index) + 1
-        word_frequencies = self.tokenizer.context_index.values()
+        context_frequencies = list(self.tokenizer.context_frequencies)
         graph = tf.Graph()
-        
+
         with graph.as_default():
 
           # Input data.
           sampling_table = self._make_sampling_table(vocab_length)
           self.custom_runner = CustomRunner(self.batch_size, self.skipgrams_generator(sampling_table))
           words_ph, contexts_ph = self.custom_runner.get_inputs()
-        
+
           # Ops and variables pinned to the CPU because of missing GPU implementation
           with tf.device('/cpu:0'):
             # Declare all variables we need.
@@ -157,24 +162,24 @@ class SkipGram(object):
                     [vocab_length, self.embedding_size], -init_width, init_width),
                 name="emb")
             self._emb = emb
-        
+
             # Softmax weight: [vocab_size, emb_dim]. Transposed.
             sm_w_t = tf.Variable(
                 tf.zeros([vocab_length, self.embedding_size]),
                 name="sm_w_t")
-        
+
             # Softmax bias: [vocab_size].
             sm_b = tf.Variable(tf.zeros([vocab_length]), name="sm_b")
-        
+
             # Global step: scalar, i.e., shape [].
             self.global_step = tf.Variable(0, name="global_step")
-        
+
             # Nodes to compute the nce loss w/ candidate sampling.
             contexts_matrix = tf.reshape(
                 tf.cast(contexts_ph,
                         dtype=tf.int64),
                 [self.batch_size, 1])
-        
+
             # Negative sampling.
             sampled_ids, _, _ = (tf.nn.fixed_unigram_candidate_sampler(
                 true_classes=contexts_matrix,
@@ -183,24 +188,24 @@ class SkipGram(object):
                 unique=True,
                 range_max=vocab_length,
                 distortion=0.75,
-                unigrams= [max(word_frequencies)] + word_frequencies))
-        
+                unigrams= [max(context_frequencies)] + context_frequencies))
+
             # Embeddings for examples: [batch_size, emb_dim]
             example_emb = tf.nn.embedding_lookup(emb, words_ph)
-        
+
             # Weights for labels: [batch_size, emb_dim]
             true_w = tf.nn.embedding_lookup(sm_w_t, contexts_ph)
             # Biases for labels: [batch_size, 1]
             true_b = tf.nn.embedding_lookup(sm_b, contexts_ph)
-        
+
             # Weights for sampled ids: [num_sampled, emb_dim]
             sampled_w = tf.nn.embedding_lookup(sm_w_t, sampled_ids)
             # Biases for sampled ids: [num_sampled, 1]
             sampled_b = tf.nn.embedding_lookup(sm_b, sampled_ids)
-        
+
             # True logits: [batch_size, 1]
             true_logits = tf.reduce_sum(tf.multiply(example_emb, true_w), 1) + true_b
-        
+
             # Sampled logits: [batch_size, num_sampled]
             # We replicate sampled noise labels for all examples in the batch
             # using the matmul.
@@ -208,7 +213,7 @@ class SkipGram(object):
             sampled_logits = tf.matmul(example_emb,
                                        sampled_w,
                                        transpose_b=True) + sampled_b_vec
-                                       
+
             true_xent = tf.nn.sigmoid_cross_entropy_with_logits(
                     labels=tf.ones_like(true_logits), logits=true_logits)
             sampled_xent = tf.nn.sigmoid_cross_entropy_with_logits(
@@ -220,10 +225,11 @@ class SkipGram(object):
                                tf.reduce_sum(sampled_xent)) / self.batch_size
             tf.summary.scalar("NCE_loss", loss)
             self._loss = loss
-            
-            decay_steps = self.tokenizer.words_per_epoch * self.epochs / self.batch_size           
+
+            decay_steps = sum(numpy.array(context_frequencies) * sampling_table[1:]) * \
+                        self.epochs / self.batch_size
             lr = tf.train.polynomial_decay(self.learning_rate, self.global_step,
-                                           decay_steps, end_learning_rate=0.00002, power=1.0)
+                                           int(decay_steps), end_learning_rate=0.00002, power=1.0)
             self._lr = lr
             optimizer = tf.train.GradientDescentOptimizer(lr)
             train = optimizer.minimize(loss,
@@ -233,7 +239,7 @@ class SkipGram(object):
         return graph
 
     def train(self):
-    
+
         with tf.Session(graph=self.prepare_model()) as session:
           # We must initialize all variables before we use them.
           tf.global_variables_initializer().run()
@@ -243,38 +249,48 @@ class SkipGram(object):
           # start our custom queue runner's threads
           self.custom_runner.start_threads(session)
           print('Variables initialized and queues running')
-          
+
           average_loss = 0
-          i = 0
+          current_epoch = self.corpus._epoch
+
           while True:
-              i += 1
+              if self.corpus._epoch > current_epoch:
+                  self.save_embeddings("embeddings_{}.txt".format(current_epoch))
+                  current_epoch = self.corpus._epoch
               if self.corpus._epoch > self.epochs:
-                  self.save_embeddings("embeddings.pkl")
                   break
-              
+
               # We perform one update step by evaluating the optimizer op (including it
               # in the list of returned values for session.run()
-              _, loss_val = session.run([self._optimizer, self._loss])
+              _, loss_val, lr, step = session.run(
+                      [self._optimizer, self._loss, self._lr, self.global_step])
               average_loss += loss_val
-            
-              if i % 2000 == 0 and i > 0:
-                average_loss /= 2000
+
+              if step % 10000 == 0 and step > 0:
+                average_loss /= 10000
                 # The average loss is an estimate of the loss over the last 2000 batches.
-                print('Average loss at step ', i, 'epoch', self.corpus._epoch, ': ', average_loss)
+                logging.info('Epoch {} step {}: lr {} average loss {}'.format(
+                        self.corpus._epoch, step, lr, average_loss))
                 average_loss = 0
           session.run(self.custom_runner.queue.close(cancel_pending_enqueues=True))
           coord.request_stop()
           coord.join(threads)
-                  
+
     def save_embeddings(self, path):
-        """ Saves only the word embeddings along with the dictionary """
-        with open(path, 'wb') as fout:
-            pkl.dump([self._emb.eval(),
-                      self.tokenizer.word_index], fout, -1)
+        logging.info("Saving the embeddings")
+        if not os.path.exists("embeddings"):
+            os.makedirs("embeddings")
+        with open("embeddings/" + path, 'w') as fout:
+            embs = self._emb.eval()
+            for k in self.tokenizer.word_index.keys():
+                i = self.tokenizer.word_index[k]
+                vector = embs[i]
+                l = " ".join([k] + [str(d) for d in vector])
+                fout.write(l + "\n")
 
 if __name__ == "__main__":
-    SG = SkipGram(corpus_path="en-ud-train.conllu", 
-                  embedding_size=300, num_words=100000, negative_samples=15, epochs=15,
-                  learning_rate=0.2, batch_size=16, labeled=True)
+    SG = SkipGram(corpus_path=FLAGS.corpus_path, embedding_size=FLAGS.embedding_size,
+        min_freq=FLAGS.min_freq, negative_samples=FLAGS.negative_samples, epochs=FLAGS.epochs,
+        learning_rate=FLAGS.learning_rate, batch_size=FLAGS.batch_size, labeled=FLAGS.labeled)
     SG.tokenize_corpus()
     SG.train()
